1e8914a0ad185dc09b981e0c742b1ecea87714d6
Brian Stern <brian.stern2511@gmail.com>
2024-10-18 14:25:34 -0400

    adding header to dashboard route

--- app/ui/Header.jsx
+++ app/ui/Header.jsx
@@ -1,68 +1,81 @@
-"use client";
-import React, { useState, useEffect } from "react";
-import { useAuth0 } from "@auth0/auth0-react";
-import axios from "axios";
-import Button from "@mui/material/Button";
-import PropTypes from "prop-types";
-import logo from "./assets/ailogo.png";
-import slogan from "./assets/slogan.png";
-import {
-  HeaderContainerStyled,
-  LogoStyled,
-  SloganStyled,
-  LogoutButtonContainerStyled,
-  ProfileImageStyled,
-  VisitCounterStyled,
-} from "./HeaderStyledComponents";
+// "use client";
+// import React, { useState, useEffect } from "react";
+// import { useAuth0 } from "@auth0/auth0-react";
+// import axios from "axios";
+// import Button from "@mui/material/Button";
+// import PropTypes from "prop-types";
+import logo from "../lib/assets/ailogo.png";
+import slogan from "../lib/assets/slogan.png";
+import Image from "next/image";
+// import {
+//   HeaderContainerStyled,
+//   LogoStyled,
+//   SloganStyled,
+//   LogoutButtonContainerStyled,
+//   ProfileImageStyled,
+//   VisitCounterStyled,
+// } from "./HeaderStyledComponents";
 
 const Header = ({ setUserObject }) => {
-  const { logout, isAuthenticated, user } = useAuth0();
+  // const { logout, isAuthenticated, user } = useAuth0();
 
-  const [visitCount, setVisitCount] = useState(null);
+  // const [visitCount, setVisitCount] = useState(null);
 
-  useEffect(() => {
-    if (user) {
-      axios
-        .get(`/update?name=${user.name}&email=${user.email}`)
-        .then((response) => {
-          setUserObject(response.data);
-          setVisitCount(response.data.visitCount);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-  }, [isAuthenticated]);
+  // useEffect(() => {
+  //   if (user) {
+  //     axios
+  //       .get(`/update?name=${user.name}&email=${user.email}`)
+  //       .then((response) => {
+  //         setUserObject(response.data);
+  //         setVisitCount(response.data.visitCount);
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //       });
+  //   }
+  // }, [isAuthenticated]);
 
-  const logOut = () => {
-    logout({ logoutParams: { returnTo: window.location.origin } });
-  };
+  // const logOut = () => {
+  //   logout({ logoutParams: { returnTo: window.location.origin } });
+  // };
 
   return (
-    <HeaderContainerStyled>
-      <LogoStyled src={logo} alt="Company Logo" />
-      <SloganStyled src={slogan} alt="Company Slogan" />
-      {isAuthenticated && (
-        <LogoutButtonContainerStyled>
-          {user?.picture && (
-            <>
-              <ProfileImageStyled src={user.picture} alt="Profile" />
-              <VisitCounterStyled>
-                Total Visits: {visitCount}
-              </VisitCounterStyled>
-            </>
-          )}
-          <Button onClick={logOut} variant="outlined" href="#outlined-buttons">
-            Log Out
-          </Button>
-        </LogoutButtonContainerStyled>
-      )}
-    </HeaderContainerStyled>
+    <header className="bg-gray-700 p-10 flex justify-center items-center border-b-4 border-black relative">
+      <Image
+        src={logo}
+        height={75}
+        // className="hidden md:block"
+        alt="AI Companion Logo"
+      />
+      <Image
+        src={slogan}
+        height={75}
+        // className="hidden md:block"
+        alt=""
+      />
+    </header>
+
+    //   {isAuthenticated && (
+    //     <LogoutButtonContainerStyled>
+    //       {user?.picture && (
+    //         <>
+    //           <ProfileImageStyled src={user.picture} alt="Profile" />
+    //           <VisitCounterStyled>
+    //             Total Visits: {visitCount}
+    //           </VisitCounterStyled>
+    //         </>
+    //       )}
+    //       <Button onClick={logOut} variant="outlined" href="#outlined-buttons">
+    //         Log Out
+    //       </Button>
+    //     </LogoutButtonContainerStyled>
+    //   )}
+    // </HeaderContainerStyled>
   );
 };
 
-Header.propTypes = {
-  setUserObject: PropTypes.func.isRequired,
-};
+// Header.propTypes = {
+//   setUserObject: PropTypes.func.isRequired,
+// };
 
 export default Header;
