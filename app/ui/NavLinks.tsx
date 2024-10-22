7cda0e5d1e5add32361065421853d9ff51036082
Brian Stern <brian.stern2511@gmail.com>
2024-10-22 10:31:09 -0400

    added icons and change text color

--- app/ui/NavLinks.tsx
+++ app/ui/NavLinks.tsx
@@ -1,10 +1,13 @@
 "use client";
 
-// import {
-//   UserGroupIcon,
-//   HomeIcon,
-//   DocumentDuplicateIcon,
-// } from "@heroicons/react/24/outline";
+import {
+  DocumentIcon,
+  HomeIcon,
+  DocumentCheckIcon,
+  MicrophoneIcon,
+  PhotoIcon,
+  QuestionMarkCircleIcon,
+} from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
@@ -12,29 +15,31 @@ import clsx from "clsx";
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: "Home", href: "/dashboard" },
-  // { name: "Home", href: "/dashboard", icon: HomeIcon },
+  { name: "Home", href: "/dashboard", icon: HomeIcon },
   {
     name: "Update Resume",
     href: "/dashboard/update-resume",
-    // icon: DocumentDuplicateIcon,
+    icon: DocumentIcon,
   },
-  //   { name: "Customers", href: "/dashboard/customers", icon: UserGroupIcon },
-  { name: "Create Cover Letter", href: "/dashboard/cover-letter" },
+  {
+    name: "Create Cover Letter",
+    href: "/dashboard/cover-letter",
+    icon: DocumentCheckIcon,
+  },
   {
     name: "Transcribe Audio from Video",
     href: "/dashboard/transcribe-audio",
-    // icon: DocumentDuplicateIcon,
+    icon: MicrophoneIcon,
   },
   {
     name: "Create AI Generated Images",
     href: "/dashboard/image-generation",
-    // icon: DocumentDuplicateIcon,
+    icon: PhotoIcon,
   },
   {
     name: "Create SQL Queries",
     href: "/dashboard/sql-queries",
-    // icon: DocumentDuplicateIcon,
+    icon: QuestionMarkCircleIcon,
   },
 ];
 
@@ -44,20 +49,20 @@ export default function NavLinks() {
   return (
     <>
       {links.map((link) => {
-        // const LinkIcon = link.icon;
+        const LinkIcon = link.icon;
         return (
           <Link
             key={link.name}
             href={link.href}
             className={clsx(
-              "flex h-[48px] grow items-center justify-center gap-1 rounded-md p-1 text-base font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+              "flex h-[48px] grow items-center justify-center gap-1 rounded-md p-1 text-base font-medium hover:bg-mainPageGray hover:text-black md:flex-none md:justify-start md:p-2 md:px-3",
               {
-                "bg-sky-100 text-blue-600": pathname === link.href,
+                "bg-mainPageGray text-black": pathname === link.href,
               },
               "mb-1"
             )}
           >
-            {/* <LinkIcon className="w-6" /> */}
+            {LinkIcon && <LinkIcon className="w-6" />}
             <p>{link.name}</p>
           </Link>
         );
