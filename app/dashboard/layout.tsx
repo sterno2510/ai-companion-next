04bd5dcb3a9eb39e9213cca84c5bfd27d72fcd22
Brian Stern <brian.stern2511@gmail.com>
2024-10-22 10:53:20 -0400

    fix screen width

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -7,14 +7,14 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <Header />
       <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
         <div
-          className="bg-sideBarGray w-full flex-none md:w-72 w-1/8 text-white p-5 border-r-4 border-black"
+          className="bg-sideBarGray w-full flex-none md:w-80 w-1/8 text-white p-5 border-r-4 border-black"
           data-testid="sidebar"
         >
-          <div className="py-3 text-center">Menu</div>
-          <hr className="pb-2" />
+          <div className="py-3 text-center">MENU</div>
+          <hr className="pb-3" />
           <NavLinks />
         </div>
-        <div className="bg-mainPageGray flex-grow p-6 md:overflow-y-auto md:p-12">
+        <div className="bg-mainPageGray flex-grow p-6 md:overflow-y-auto md:p-9">
           {children}
         </div>
       </div>
