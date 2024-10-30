e0f2e46aacc018a250be151195fdade340239a70
Brian Stern <brian.stern2511@gmail.com>
2024-10-30 11:41:42 -0400

    protected dashboard page

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,23 +1,27 @@
 import Header from "../ui/Header";
 import NavLinks from "../ui/NavLinks";
+import { withPageAuthRequired, getSession } from "@auth0/nextjs-auth0";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div>
-      <Header login={true} />
-      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-        <div
-          className="bg-sideBarGray w-full flex-none md:w-80 w-1/8 text-white p-5 border-r-4 border-black"
-          data-testid="sidebar"
-        >
-          <div className="py-3 text-center">MENU</div>
-          <hr className="pb-3" />
-          <NavLinks />
-        </div>
-        <div className="bg-mainPageGray flex-grow p-6 md:overflow-y-auto md:p-9">
-          {children}
+export default withPageAuthRequired(
+  function Layout({ children }: { children: React.ReactNode }) {
+    return (
+      <div>
+        <Header login={true} />
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+          <div
+            className="bg-sideBarGray w-full flex-none md:w-80 w-1/8 text-white p-5 border-r-4 border-black"
+            data-testid="sidebar"
+          >
+            <div className="py-3 text-center">MENU</div>
+            <hr className="pb-3" />
+            <NavLinks />
+          </div>
+          <div className="bg-mainPageGray flex-grow p-6 md:overflow-y-auto md:p-9">
+            {children}
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  },
+  { returnTo: "/" }
+);
